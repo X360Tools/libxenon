a6fe4c1f14921b5512e3cc6e400971e4d977b75e
Josh Davidson <joshuadavidson2000@gmail.com>
2021-12-31 14:19:26 -0500

    Revert "Rewrite the ELF loader / harden it against bad files"
    This reverts commit 7bf04824d2e7162688a6e0106523b8cd1001614f.

--- libxenon/drivers/pci/io.h
+++ libxenon/drivers/pci/io.h
@@ -3,24 +3,10 @@
 
 #include <stdint.h>
 
-static inline __attribute__((always_inline)) uint32_t read32(long addr)
-{
-	return __builtin_bswap32(*(volatile uint32_t*)addr);
-}
+uint32_t read32(long addr);
+uint32_t read32n(long addr);
 
-static inline __attribute__((always_inline)) uint32_t read32n(long addr)
-{
-	return *(volatile uint32_t*)addr;
-}
-
-static inline __attribute__((always_inline)) void write32(long addr, uint32_t val)
-{
-	*(volatile uint32_t*)addr = __builtin_bswap32(val);
-}
-
-static inline __attribute__((always_inline)) void write32n(long addr, uint32_t val)
-{
-	*(volatile uint32_t*)addr = val;
-}
+void write32(long addr, uint32_t val);
+void write32n(long addr, uint32_t val);
 
 #endif
